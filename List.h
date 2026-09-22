335fd4c7420683405b03404f7cb8100522347654
Phong Tran <77176227+phongtran0117@users.noreply.github.com>
2026-09-22 15:16:14 -0700

    step 6: swap to to makelist

--- List.h
+++ List.h
@@ -21,7 +21,7 @@ public:
 
 template <typename T>
 std::unique_ptr<List<T>> makeList() {
-    return std::make_unique<LinkedList<T>>();
-    // return std::make_unique<ArrayList<T>>();
+    // return std::make_unique<LinkedList<T>>();
+    return std::make_unique<ArrayList<T>>();
 }
 
